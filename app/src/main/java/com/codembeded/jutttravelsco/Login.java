a34f4aeeeb8bb98c019e954025616f213bfdaaa4
Amir Rajpoot <AmirRajppot536@github.com>
2021-03-01 16:38:05 +0500

    Amir's Commit

--- app/src/main/java/com/codembeded/jutttravelsco/Login.java
+++ app/src/main/java/com/codembeded/jutttravelsco/Login.java
@@ -2,13 +2,25 @@ package com.codembeded.jutttravelsco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    TextView Sign_up_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Sign_up_btn = findViewById(R.id.signUp_login_tv);
+        Sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
