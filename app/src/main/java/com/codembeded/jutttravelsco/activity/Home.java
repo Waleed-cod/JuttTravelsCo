5c7ceb185e74db91357df13969d847f302359e2c
waleed-cod <ranawlaee@gmail.com>
2021-03-02 19:52:07 +0500

    Navigation done Commit

--- app/src/main/java/com/codembeded/jutttravelsco/activity/Home.java
+++ app/src/main/java/com/codembeded/jutttravelsco/activity/Home.java
@@ -2,12 +2,15 @@ package com.codembeded.jutttravelsco.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.codembeded.jutttravelsco.R;
 import com.codembeded.jutttravelsco.adapter.AdapterForHomeSlider;
 import com.codembeded.jutttravelsco.models.HomeImageSliderModels;
+import com.google.android.material.card.MaterialCardView;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
@@ -18,8 +21,10 @@ import java.util.List;
 public class Home extends AppCompatActivity {
 
     private SliderView imageSlider;
-    private ArrayList<HomeImageSliderModels> items = new ArrayList<>();
+    private final ArrayList<HomeImageSliderModels> items = new ArrayList<>();
     private AdapterForHomeSlider imageSliderAdapter;
+
+    MaterialCardView bookMyTickets_cv, bookMyRide_cv, myTicket_cv, tour_cv, history_cv, complain_cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +32,69 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         imageSlider = findViewById(R.id.home_image_slider);
+        bookMyTickets_cv = findViewById(R.id.book_my_tickets_home);
+        bookMyRide_cv = findViewById(R.id.book_my_ride_home);
+        myTicket_cv = findViewById(R.id.my_tickets_home);
+        tour_cv = findViewById(R.id.tour_home);
+        history_cv = findViewById(R.id.history_home);
+        complain_cv = findViewById(R.id.complain_home);
 
         setImageSlider();
+        OnClicks();
 
+    }
+
+    private void OnClicks() {
+        bookMyTickets_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, BookMyTicket.class);
+                startActivity(intent);
+            }
+        });
+
+        bookMyRide_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Home.this, BookMyRide.class);
+                startActivity(i);
+            }
+        });
+        myTicket_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Home.this, BookMyTicket.class);
+                startActivity(i);
+            }
+        });
+        history_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Home.this, MyHistory.class);
+                startActivity(i);
+            }
+        });
+        tour_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Home.this, Tours.class);
+                startActivity(i);
+            }
+        });
+        complain_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Home.this, Complain.class);
+                startActivity(i);
+            }
+        });
     }
 
     private void setImageSlider() {
 
         items.add(new HomeImageSliderModels(R.drawable.world_tour));
         items.add(new HomeImageSliderModels(R.drawable.world_tour_beauty));
-        imageSliderAdapter = new AdapterForHomeSlider(items,Home.this);
+        imageSliderAdapter = new AdapterForHomeSlider(items, Home.this);
         imageSlider.setSliderAdapter(imageSliderAdapter);
         imageSlider.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
         imageSlider.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
--- app/src/main/java/com/codembeded/jutttravelsco/activity/Login.java
+++ app/src/main/java/com/codembeded/jutttravelsco/activity/Login.java
@@ -5,25 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.codembeded.jutttravelsco.R;
 
 public class Login extends AppCompatActivity {
-    TextView Sign_up_btn;
+    TextView sign_up_tv, forgot_password_tv;
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Sign_up_btn = findViewById(R.id.signUp_login_tv);
-        Sign_up_btn.setOnClickListener(new View.OnClickListener() {
+        sign_up_tv = findViewById(R.id.signUp_login_tv);
+        forgot_password_tv = findViewById(R.id.forgot_password_login_tv);
+        login_btn = findViewById(R.id.logIn_btn);
+
+        sign_up_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, SignUp.class);
                 startActivity(intent);
             }
         });
+
+        forgot_password_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, ForgetPassword.class);
+                startActivity(i);
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
