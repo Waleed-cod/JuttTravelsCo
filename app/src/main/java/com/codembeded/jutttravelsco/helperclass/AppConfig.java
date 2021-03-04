b3b1081176ea6a2e31415af4b3eecafb4d82f2f6
waleed-cod <ranawlaee@gmail.com>
2021-03-04 13:08:09 +0500

    volley Commit

---
+++ app/src/main/java/com/codembeded/jutttravelsco/helperclass/AppConfig.java
@@ -0,0 +1,8 @@
+package com.codembeded.jutttravelsco.helperclass;
+
+public class AppConfig {
+
+    public static final String BASE_URL = "http://jutttravels.codembeded.com/mobile_api/";
+    public static final String GET_SLIDER_IMAGE = BASE_URL + "get_slider_images.php";
+    public static final String LOG_IN = BASE_URL + "login.php";
+}
